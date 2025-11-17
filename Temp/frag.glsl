ff015fd7d534b24d8d164d839a3fc936831d48de
Russo <yaleksander42@gmail.com>
2025-11-17 10:41:10 -0300

    implementação das sombras da aula passada

---
+++ Temp/frag.glsl
@@ -0,0 +1,144 @@
+#include <common>
+#include <packing>
+#include <lights_pars_begin>
+#include <shadowmap_pars_fragment>
+
+struct Material
+{
+	vec3 specular;
+	float shininess;
+};
+
+uniform sampler2D myTexture;
+uniform sampler2D normalMap;
+uniform vec3 cameraPos;
+uniform bool useTexture;
+uniform bool useNormalMap;
+uniform vec3 noTexColor;
+uniform Material material;
+
+varying vec2 vUV;
+varying vec3 fragPos;
+varying mat3 TBN;
+
+void main()
+{
+	vec3 ambient = ambientLightColor, phong = vec3(0.0);
+	vec3 lightDir, viewDir, reflectDir;	
+	float diff, spec, dist, att, shadow;
+	vec3 normal = useNormalMap ? normalize(TBN * (texture2D(normalMap, vUV).rgb * 2.0 - 1.0)) : normalize(TBN * vec3(0.0, 0.0, 1.0));
+
+	#if NUM_DIR_LIGHTS > 0
+
+		#pragma unroll_loop_start
+		for (int i = 0; i < NUM_DIR_LIGHTS; i++)
+		{
+			// diffuse
+			lightDir = normalize(directionalLights[0].direction);
+			diff = max(0.0, dot(normal, lightDir));
+
+			// specular
+			viewDir = normalize(cameraPos - fragPos);
+			reflectDir = reflect(-lightDir, normal);
+			spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+
+			// shadow
+			shadow = 1.0;
+			#if UNROLLED_LOOP_INDEX < NUM_DIR_LIGHT_SHADOWS
+				shadow = receiveShadow ? getShadow(
+					directionalShadowMap[i],
+					directionalLightShadows[i].shadowMapSize,
+					directionalLightShadows[i].shadowIntensity,
+					directionalLightShadows[i].shadowBias,
+					directionalLightShadows[i].shadowRadius,
+					vDirectionalShadowCoord[i]
+				) : 1.0;
+			#endif
+
+			// phong
+			phong += (diff + material.specular * spec) * directionalLights[0].color * shadow;
+		}
+		#pragma unroll_loop_end
+
+	#endif
+
+	#if NUM_POINT_LIGHTS > 0
+
+		#pragma unroll_loop_start
+		for (int i = 0; i < NUM_POINT_LIGHTS; i++)
+		{
+			dist = length(pointLights[i].position - fragPos);
+			att = getDistanceAttenuation(dist, pointLights[i].distance, pointLights[0].decay);
+
+			// diffuse
+			lightDir = normalize(pointLights[i].position - fragPos);
+			diff = max(0.0, dot(normal, lightDir));
+
+			// specular
+			viewDir = normalize(cameraPos - fragPos);
+			reflectDir = reflect(-lightDir, normal);
+			spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+
+			// shadow
+			shadow = 1.0;
+			#if UNROLLED_LOOP_INDEX < NUM_POINT_LIGHT_SHADOWS
+				shadow = receiveShadow ? getPointShadow(
+					pointShadowMap[i],
+					pointLightShadows[i].shadowMapSize,
+					pointLightShadows[i].shadowIntensity,
+					pointLightShadows[i].shadowBias,
+					pointLightShadows[i].shadowRadius,
+					vPointShadowCoord[i],
+					pointLightShadows[i].shadowCameraNear,
+					pointLightShadows[i].shadowCameraFar
+				) : 1.0;
+			#endif
+
+			// phong
+			phong += (diff + material.specular * spec) * pointLights[i].color * att * shadow;
+		}
+		#pragma unroll_loop_end
+
+	#endif
+
+	#if NUM_SPOT_LIGHTS > 0
+
+		#pragma unroll_loop_start
+		for (int i = 0; i < NUM_SPOT_LIGHTS; i++)
+		{
+			dist = length(spotLights[0].position - fragPos);
+			lightDir = normalize(spotLights[0].position - fragPos);
+			att = getDistanceAttenuation(dist, spotLights[0].distance, spotLights[0].decay);
+			att *= getSpotAttenuation(spotLights[0].coneCos, spotLights[0].penumbraCos, dot(lightDir, spotLights[0].direction));
+
+			// diffuse
+			diff = max(0.0, dot(normal, lightDir));
+
+			// specular
+			viewDir = normalize(cameraPos - fragPos);
+			reflectDir = reflect(-lightDir, normal);
+			spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+
+			// shadow
+			shadow = 1.0;
+			#if UNROLLED_LOOP_INDEX < NUM_SPOT_LIGHT_SHADOWS
+				shadow = receiveShadow ? getShadow(
+					spotShadowMap[i],
+					spotLightShadows[i].shadowMapSize,
+					spotLightShadows[i].shadowIntensity,
+					spotLightShadows[i].shadowBias,
+					spotLightShadows[i].shadowRadius,
+					vSpotLightCoord[i]
+				) : 1.0;
+			#endif
+
+			// phong
+			phong += (diff + material.specular * spec) * spotLights[0].color * att * shadow;
+		}
+		#pragma unroll_loop_end
+
+	#endif
+
+	shadow = 1.0;//getShadowMask();
+	gl_FragColor = (useTexture ? texture2D(myTexture, vUV) : vec4(noTexColor, 1.0)) * vec4(ambient + phong * shadow, 1.0);
+}
---
+++ Temp/script.js
@@ -0,0 +1,133 @@
+// https://learnopengl.com/Advanced-Lighting/Shadows/Shadow-Mapping
+// https://github.com/mrdoob/three.js/tree/master/src/renderers/shaders/ShaderChunk
+
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+var camera, scene, renderer, spotTarget, dirTarget, rot, box, controls;
+
+init();
+
+async function init()
+{
+	const vert = await (await fetch("vert.glsl")).text();
+	const frag = await (await fetch("frag.glsl")).text();
+
+	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 100);
+	camera.position.set(0, 2, 2);
+	scene = new THREE.Scene();
+	scene.add(camera);
+	scene.add(new THREE.AmbientLight(0x777777));
+
+	const textureLoader = new THREE.TextureLoader();
+	const geometry = new THREE.BoxGeometry(1, 1, 1);
+	geometry.computeTangents();
+	const material = new THREE.ShaderMaterial(
+	{
+		uniforms: THREE.UniformsUtils.merge(
+		[
+			THREE.UniformsLib["lights"],
+			{
+				cameraPos: { value: camera.position },
+				myTexture: { value: textureLoader.load("assets/goose.png") },
+				normalMap: { value: textureLoader.load("assets/normal_map.png") },
+				useNormalMap: { value: true },
+				useTexture: { value: true },
+				noTexColor: { value: new THREE.Color(0, 0, 0) },
+
+				material:
+				{
+					value:
+					{
+						specular: new THREE.Vector3(0.75, 0.75, 0.75),
+						shininess: 16
+					}
+				}
+			}
+		]),
+		vertexShader: vert,
+		fragmentShader: frag,
+		lights: true
+	});
+
+	rot = 0;
+	dirTarget = new THREE.Object3D();
+	spotTarget = new THREE.Object3D();
+	scene.add(dirTarget);
+	scene.add(spotTarget);
+
+	const s = 3;
+	box = new THREE.Mesh(geometry, material.clone());
+	box.position.set(0, 0.5, 0);
+	box.castShadow = true;
+	scene.add(box);
+
+	const planeGeo = new THREE.PlaneGeometry(10, 10);
+	const planeMat = material.clone();
+	planeMat.uniforms.useTexture.value = false;
+	planeMat.uniforms.useNormalMap.value = false;
+	planeMat.uniforms.noTexColor.value = new THREE.Color(0.8, 0.8, 0.8);
+	const floor = new THREE.Mesh(planeGeo, planeMat);
+	floor.castShadow = floor.receiveShadow = true;
+	floor.rotation.set(-Math.PI / 2, 0, 0);
+	scene.add(floor);
+
+	const dirLight = new THREE.DirectionalLight(0xffffff, 0.25);
+	dirLight.position.set(-5, 5, -5);
+	dirLight.target = dirTarget;
+	dirLight.castShadow = true;
+	const d = 10;
+	dirLight.shadow.camera.left = -d;
+	dirLight.shadow.camera.right = d;
+	dirLight.shadow.camera.top = d;
+	dirLight.shadow.camera.bottom = -d;
+	dirLight.shadow.camera.far = d * 2;
+	dirLight.shadow.bias = 0.02;
+	dirLight.shadow.mapSize = new THREE.Vector2(4096, 4096);
+	scene.add(dirLight);
+    const cameraHelper = new THREE.CameraHelper(dirLight.shadow.camera);
+    scene.add(cameraHelper);
+
+	const pointLight1 = new THREE.PointLight(0xffffff, 3.0);
+	pointLight1.position.set(-3, 2, 0);
+	pointLight1.castShadow = true;
+	scene.add(pointLight1);
+	const pointLight2 = new THREE.PointLight(0xffffff, 3.0);
+	pointLight2.position.set(3, 2, 0);
+	pointLight2.castShadow = true;
+	scene.add(pointLight2);
+
+	const spotLight = new THREE.SpotLight(0xffffff, 4.0);
+	spotLight.target = spotTarget;
+	spotLight.position.set(3, 3, 3);
+	spotLight.penumbra = 0.15;
+	spotLight.castShadow = true;
+	scene.add(spotLight);
+
+	renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer.setClearColor(0xffffff, 1);
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setAnimationLoop(animate);
+	renderer.shadowMap.enabled = true;
+	document.body.appendChild(renderer.domElement);
+
+	controls = new OrbitControls(camera, renderer.domElement);
+	controls.maxPolarAngle = Infinity;
+	controls.enablePan = false;
+
+	window.addEventListener("resize", onWindowResize);
+}
+
+function onWindowResize()
+{
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate()
+{
+	controls.update();
+	renderer.render(scene, camera);
+}
